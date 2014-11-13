e42b85bfe26a35c468a222c2eaea90fe91c846e8
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-14 01:24:46 +0300

    add tests

--- test/avl_tree_SUITE.erl
+++ test/avl_tree_SUITE.erl
@@ -21,6 +21,7 @@ groups() ->
                ]},
      {read, [], [
                  new_tree,
+                 lookup4,
                  lookup3,
                  lookup2,
                  lookup1
@@ -54,6 +55,19 @@ lookup3(_Config) ->
     Tree = {2, Subtree2},
     {value, a} = avl_tree:lookup(1, Tree),
     {value, b} = avl_tree:lookup(2, Tree),
+    ok.
+
+lookup4(_Config) ->
+    Subtree1 = {1, {1, a}, nil, nil},
+    Subtree2 = {2, {5, b}, Subtree1, nil},
+    Subtree3 = {1, {50, c}, nil, nil},
+    Subtree4 = {3, {10, d}, Subtree2, Subtree3},
+    Tree = {4, Subtree4},
+    none = avl_tree:lookup(2, Tree),
+    {value, a} = avl_tree:lookup(1, Tree),
+    {value, b} = avl_tree:lookup(5, Tree),
+    {value, d} = avl_tree:lookup(10, Tree),
+    {value, c} = avl_tree:lookup(50, Tree),
     ok.
 
 %% ===================================================================
