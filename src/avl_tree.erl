441c2e2a49e03f3fe8af7532af9d281e9daec8fc
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-15 19:51:44 +0300

    adding insert

--- src/avl_tree.erl
+++ src/avl_tree.erl
@@ -1,6 +1,7 @@
 -module(avl_tree).
 
 -export([
+         insert/3,
          size/1,
          lookup/2,
          new/0
@@ -45,4 +46,89 @@ subtree_lookup(Key, {_, _, _, Right}) ->
 
 size({Size, _}) ->
     Size.
+
+%% assume the key is _not_ in the tree
+-spec insert(Key, Value, Tree) -> Tree2 when
+      Tree :: tree(),
+      Tree2 :: tree(),
+      Key :: key(),
+      Value :: value().
+
+insert(Key, Value, {Size, Subtree}) ->
+    Subtree2 = insert1(Key, Value, Subtree),
+    {Size + 1, Subtree2}.
+
+insert1(Key, Value, {_, {Key0, _}=Root, Smaller, Bigger})
+  when Key < Key0 ->
+    Smaller2 = insert1(Key, Value, Smaller),
+    Height2 = calc_new_height(Smaller2, Bigger),
+    Balance = height(Smaller2) - height(Bigger),
+    New_subtree = {Height2, Root, Smaller2, Bigger},
+    case is_balanced(Balance) of
+        true ->
+            New_subtree;
+        false ->
+            rebalance(Balance, New_subtree)
+    end;
+insert1(Key, Value, {Height, {Key0, _}, Smaller, Bigger}) when Key > Key0 ->
+    erlang:error(not_implemented);
+insert1(Key, Value, nil) ->
+    new_subtree(Key, Value);
+insert1(Key, _, _) ->
+    erlang:error({key_exists, Key}).
+
+rebalance(2, {_, _, Smaller, _} = Subtree) ->
+    case find_longer_side(Smaller) of
+        left ->
+            left_left_rotation(Subtree);
+        right ->
+            left_right_rotation(Subtree)
+    end;
+rebalance(Balance, {Height, {Key, Value}, Smaller, Bigger}) ->
+    erlang:error(not_implemented).
+
+new_subtree(Key, Val) ->
+    {0, new_root_node(Key, Val), nil, nil}.
+
+new_root_node(Key, Val) ->
+    {Key, Val}.
+
+height({Height, _, _, _}) ->
+    Height.
+
+is_balanced(Height) when Height >= -1, Height =< 1 ->
+    true;
+is_balanced(_) ->
+    false.
+
+find_longer_side(Subtree) ->
+    which_longer(get_smaller_subtree(Subtree), get_bigger_subtree(Subtree)).
+
+get_smaller_subtree({_, _, Smaller, _}) ->
+    Smaller.
+
+get_bigger_subtree({_, _, _, Bigger}) ->
+    Bigger.
+
+which_longer(Subtree1, Subtree2) ->
+    H1 = height(Subtree1),
+    H2 = height(Subtree2),
+    if H1 > H2 ->
+            left;
+       H1 < H2 ->
+            right;
+       true ->
+            equal
+    end.
+
+calc_new_height(Subtree1, Subtree2) ->
+    H1 = height(Subtree1),
+    H2 = height(Subtree2),
+    erlang:max(H1, H2) + 1.
+
+left_left_rotation(Subtree) ->
+    erlang:error(not_implemented).
+
+left_right_rotation(Subtree) ->
+    erlang:error(not_implemented).
 
