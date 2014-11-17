16415038bdb60537d1b9a33c471ba2a68afe1129
Arkadiy Drovosekov <arkdro@gmail.com>
2014-11-17 22:26:49 +0300

    test with random data

--- test/many_SUITE.erl
+++ test/many_SUITE.erl
@@ -28,6 +28,7 @@ groups() ->
     ].
 
 init_per_suite(Config) ->
+    random:seed(now()),
     Local = local_config(Config),
     [{local, Local} | Config].
 
@@ -35,20 +36,26 @@ end_per_suite(_Config) ->
     ok.
 
 compare_with_sort(Config) ->
-    set_timeout(Config),
+    Dur = set_timeout(Config),
+    {Tests, Items} = compare_with_sort_till_timeout(Config, Dur),
+    ct:pal("tests: ~p, items: ~p", [Tests, Items]),
     ok.
 
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
 
-set_timeout(Config) ->
+get_local_value(Key, Config) ->
     Local = proplists:get_value(local, Config),
-    case proplists:get_value(timetrap, Local) of
+    proplists:get_value(Key, Local).
+
+set_timeout(Config) ->
+    case get_local_value(duration, Config) of
         undefined ->
-            ok;
+            30;
         Val ->
-            ct:timetrap(Val)
+            ct:timetrap({seconds, Val + 5}),
+            Val
     end.
 
 local_config(Config) ->
@@ -57,4 +64,45 @@ local_config(Config) ->
     Path = filename:join([Dir, File]),
     {ok, [L]} = file:consult(Path),
     L.
+
+compare_with_sort_till_timeout(Config, Dur) ->
+    Cur = timestamp(),
+    Stop = Cur + Dur - 1,
+    compare_with_sort_till_timeout(Config, Cur, Stop, 0, 0).
+
+compare_with_sort_till_timeout(_, Cur, Stop, Tests, Items) when Cur > Stop ->
+    {Tests, Items};
+compare_with_sort_till_timeout(Config, _, Stop, Tests, Items) ->
+    Items2 = compare_with_sort_one_item(Config),
+    compare_with_sort_till_timeout(Config, timestamp(), Stop, Tests + 1,
+                                   Items + Items2).
+
+compare_with_sort_one_item(Config) ->
+    Data = gen_data(Config),
+    Sorted = lists:sort(Data),
+    Len = length(Sorted),
+    Tree = avl_tree:from_list(Data),
+    List = avl_tree:to_list(Tree),
+    case List of
+        Sorted ->
+            Len;
+        _ ->
+            ct:log("data mismatch, len=~p~ndata:~n~p~nafter tree:~n~p~n",
+                   [Len, Sorted, List]),
+            erlang:error(data_mismatch)
+    end.
+
+timestamp() ->
+    timestamp(os:timestamp()).
+
+timestamp({MS, S, _}) ->
+    MS * 1000000 + S.
+
+gen_data(Config) ->
+    Max_len = get_local_value(max_length, Config),
+    Len = random:uniform(Max_len),
+    Max_val = get_local_value(max_value, Config),
+    Keys = [random:uniform(Max_val) || _ <- lists:duplicate(Len, true)],
+    Keys2 = sets:to_list(sets:from_list(Keys)),
+    [{Key, random:uniform(Max_val)} || Key <- Keys2].
 
